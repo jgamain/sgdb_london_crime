61d250c816004ec367d9afa733536be9f6cdcaf0
jgamain <jeanne.gamain@gmail.com>
2018-04-03 10:40:49 +0200

    adaptation au rapport

--- src/main/scala/fr/upmc_insta/stl/dar/LondonCrime.scala
+++ src/main/scala/fr/upmc_insta/stl/dar/LondonCrime.scala
@@ -39,13 +39,13 @@ object LondonCrime {
     // ****************** DANS QUELS QUARTIERS LA CRIMINALITÉ EST-ELLE LA PLUS ÉLEVÉE ? ***************** //
 
     val crimeByBorough = londonCrimes
-      .groupBy("borough","year")
+      .groupBy("major_category","borough","year")
       .sum("value")
-      .groupBy("borough")
+      .groupBy("major_category","borough")
       .avg("sum(value)")
       .withColumnRenamed("avg(sum(value))","avgCrimesPerYear")
       .withColumn("avgCrimesPerYear", $"avgCrimesPerYear".cast(IntegerType))
-      .select("borough", "avgCrimesPerYear").as("cb")
+      .select("borough", "major_category","avgCrimesPerYear")
 
     //val crimeByBoroughOrderByTotal = crimeByBorough.orderBy((desc("avgCrimesPerYear")))
 
@@ -53,33 +53,6 @@ object LondonCrime {
 
     /*EXPORT CSV*/
     exportCsv(crimeByBorough, "crimeByBorough.csv");
-
-
-    //Taux de criminalité (en pourcentage)
-    val lsoaData = readCsv(sparkSession, dataFolder + "lsoa-data_iadatasheet1.csv").as("lsoa")
-
-    val populationByBorough = londonCrimes
-      .groupBy("lsoa_code", "borough")
-      .count()
-      .select("lsoa_code", "borough")
-      .join(lsoaData, $"lsoa.Codes"===$"c.lsoa_code")
-      .groupBy("borough")
-      .sum("2011")
-      .withColumnRenamed("sum(2011)", "population")
-      .select("borough", "population").as("pb")
-
-    //populationByBorough.show(30, false)
-
-    val criminality = crimeByBorough
-      .join(populationByBorough, $"cb.borough"===$"pb.borough")
-      .withColumn("rate", (col("cb.avgCrimesPerYear") / col("pb.population"))*100)
-      .withColumn("rate", $"rate".cast(IntegerType))
-      .select("cb.borough", "avgCrimesPerYear", "population", "rate")
-
-    //criminality.show(30, false)
-
-    /*EXPORT CSV*/
-    exportCsv(criminality, "criminality.csv");
 
 
     // ****************** QUELS SONT LES CRIMES LES PLUS COURANTS ? ******************* //
@@ -178,5 +151,35 @@ object LondonCrime {
 
     //crimeByMonthAvgOrdered.show
 
+
+    // ************ TAUX DE CRIMINALITE PAR ARRONDISSEMENT (en pourcentage) ******************* //
+
+    val lsoaData = readCsv(sparkSession, dataFolder + "lsoa-data_iadatasheet1.csv").as("lsoa")
+
+    val populationByBorough = londonCrimes
+      .groupBy("lsoa_code", "borough")
+      .count()
+      .select("lsoa_code", "borough")
+      .join(lsoaData, $"lsoa.Codes"===$"c.lsoa_code")
+      .groupBy("borough")
+      .sum("2011")
+      .withColumnRenamed("sum(2011)", "population")
+      .select("borough", "population").as("pb")
+
+    //populationByBorough.show(30, false)
+
+    val criminality = crimeByBorough
+      .groupBy("borough")
+      .sum("avgCrimesPerYear")
+      .withColumnRenamed("sum(avgCrimesPerYear)", "avgCrimesPerYear").as("cb")
+      .join(populationByBorough, $"cb.borough"===$"pb.borough")
+      .withColumn("rate", (col("cb.avgCrimesPerYear") / col("pb.population"))*100)
+      .withColumn("rate", $"rate".cast(IntegerType))
+      .select("cb.borough", "avgCrimesPerYear", "population", "rate")
+
+    //criminality.show(30, false)
+
+    /*EXPORT CSV*/
+    exportCsv(criminality, "criminality.csv");
   }
 }
